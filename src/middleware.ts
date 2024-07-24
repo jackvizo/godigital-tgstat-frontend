27ca8da9f324a20b07a56ffaa28e79ca4ade66c6
Gaever <mcgyveroon@gmail.com>
2024-07-24 23:39:48 +0800

    wip

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: [
-    "/dashboard",
-    /*
-     * Запросы, которые будут проходить через middleware:
-     * Исключаем маршруты вида /api/v1, которые проверяются по специальному токену BACKEND_TOKEN,
-     * а не средствами NextAuth
-     */
-    "/((?!api/v1).*)",
-  ],
+  matcher: ["/dashboard"],
 };
