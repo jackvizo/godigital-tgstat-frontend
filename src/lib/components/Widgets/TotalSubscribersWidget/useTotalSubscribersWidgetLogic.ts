222c67541f22d67cce79ed574f9f32fc3cd4b5cb
Gaever <mcgyveroon@gmail.com>
2024-07-30 00:18:00 +0800

    wip

--- src/lib/components/Widgets/TotalSubscribersWidget/useTotalSubscribersWidgetLogic.ts
+++ src/lib/components/Widgets/TotalSubscribersWidget/useTotalSubscribersWidgetLogic.ts
@@ -1,29 +1,53 @@
-import { graphql } from "@/generated/gql";
 import { useAuth } from "@/lib/auth/use-auth";
-import { useQuery } from "@apollo/client";
 import { DashboardFilters } from "@/lib/components/DashboardFilters/useDashboardFiltersLogic";
-
-export const TOTAL_SUBSCRIBERS_QUERY = graphql(`
-  query TotalParticipants($tg_channel_ids: [bigint!]) {
-    stat_channel(where: { tg_channel_id: { _in: $tg_channel_ids } }) {
-      total_participants
-    }
-  }
-`);
+import { gql, useQuery } from "@apollo/client";
 
 export interface UseTotalSubscribersWidgetLogicProps extends DashboardFilters {}
 
+type TOTAL_SUBSCRIBERS_QUERY_RES = Record<string, { total_participants: number }[]>;
+
 export function useTotalSubscribersWidgetLogic(props: UseTotalSubscribersWidgetLogicProps) {
   const auth = useAuth();
-  const totalSubscribersQuery = useQuery(TOTAL_SUBSCRIBERS_QUERY, {
+  const TOTAL_SUBSCRIBERS_QUERY = gql(
+    props.tgChannelIds.length < 1
+      ? `
+  query Dummy {
+    stat_channel(limit: 0) {pk}
+  }
+  `
+      : `
+  query TotalParticipants {
+    ${props.tgChannelIds
+      .map(
+        (tgChannelId) => `id${tgChannelId}: stat_channel(
+          where: {
+            tg_channel_id: { _eq: ${tgChannelId} }
+          },
+          limit: 1,
+          order_by: { pk: desc }
+          ) {
+            total_participants
+          }`
+      )
+      .join(" ")}
+  }
+`
+  );
+  const totalSubscribersQuery = useQuery<TOTAL_SUBSCRIBERS_QUERY_RES>(TOTAL_SUBSCRIBERS_QUERY, {
     skip: !auth?.session?.data?.accessToken || props.tgChannelIds.length < 1,
     variables: {
       tg_channel_ids: props.tgChannelIds,
     },
   });
 
+  let totalSubscribersAmount = 0;
+
+  Object.values((totalSubscribersQuery?.data ?? {}) as TOTAL_SUBSCRIBERS_QUERY_RES).map((entry) => {
+    totalSubscribersAmount += entry?.[0]?.total_participants ?? 0;
+  });
+
   return {
     totalSubscribersQuery,
-    totalSubscribersAmount: totalSubscribersQuery?.data?.stat_channel?.[0]?.total_participants ?? 0,
+    totalSubscribersAmount,
   };
 }
