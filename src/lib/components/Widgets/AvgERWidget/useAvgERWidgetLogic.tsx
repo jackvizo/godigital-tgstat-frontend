598823008112d45a3a7535239a62c17ed2d91a84
Gaever <mcgyveroon@gmail.com>
2024-11-12 00:33:28 +0800

    wip

--- src/lib/components/Widgets/AvgERWidget/useAvgERWidgetLogic.tsx
+++ src/lib/components/Widgets/AvgERWidget/useAvgERWidgetLogic.tsx
@@ -1,45 +1,52 @@
-import { graphql } from '@/generated/gql';
-import { useAuth } from '@/lib/auth/use-auth';
-import { useQuery } from '@apollo/client';
-import { DashboardFilters } from '@/lib/components/DashboardFilters/useDashboardFiltersLogic';
+import { graphql } from "@/generated/gql";
+import { useAuth } from "@/lib/auth/use-auth";
+import { useQuery } from "@apollo/client";
+import { DashboardFilters } from "@/lib/components/DashboardFilters/useDashboardFiltersLogic";
 
 export const AVG_ER_QUERY = graphql(`
-query ERAvg($from_date: timestamp!, $to_date: timestamp!, $tg_channel_ids: [bigint!]) {
-  stat_channel_aggregate(where: {timestamp: {_gte: $from_date, _lte: $to_date}, tg_channel_id: {_in: $tg_channel_ids}}) {
-    aggregate {
-      sum {
-        total_participants
+  query ERAvg($from_date: timestamp!, $to_date: timestamp!, $tg_channel_ids: [bigint!]) {
+    stat_channel_aggregate(
+      where: { timestamp: { _gte: $from_date, _lte: $to_date }, tg_channel_id: { _in: $tg_channel_ids } }
+    ) {
+      aggregate {
+        avg {
+          total_participants
+        }
+      }
+    }
+    stat_post_aggregate(
+      where: { timestamp: { _gte: $from_date, _lte: $to_date }, tg_channel_id: { _in: $tg_channel_ids } }
+    ) {
+      aggregate {
+        avg {
+          views
+        }
       }
     }
   }
-  stat_post_aggregate(where: {timestamp: {_gte: $from_date, _lte: $to_date}, tg_channel_id: {_in: $tg_channel_ids}}) {
-    aggregate {
-      avg {
-        views
-      }
-    }
-  }
-}
 `);
 
-export interface UseAvgERWidgetLogicProps extends DashboardFilters {
-}
+export interface UseAvgERWidgetLogicProps extends DashboardFilters {}
 
 export function useAvgERWidgetLogic(props: UseAvgERWidgetLogicProps) {
   const auth = useAuth();
   const avgErQuery = useQuery(AVG_ER_QUERY, {
-    skip: !auth?.session?.data?.accessToken || props.tgChannelIds.length < 1 || !props.utcStartDatetime || !props.utcEndDatetime,
+    skip:
+      !auth?.session?.data?.accessToken ||
+      props.tgChannelIds.length < 1 ||
+      !props.utcStartDatetime ||
+      !props.utcEndDatetime,
     variables: {
       from_date: props.utcStartDatetime,
       to_date: props.utcEndDatetime,
-      tg_channel_ids: props.tgChannelIds
+      tg_channel_ids: props.tgChannelIds,
     },
   });
 
-  const usersCount = Number(avgErQuery?.data?.stat_channel_aggregate?.aggregate?.sum?.total_participants);
+  const usersCount = Number(avgErQuery?.data?.stat_channel_aggregate?.aggregate?.avg?.total_participants);
   const views = Number(avgErQuery?.data?.stat_post_aggregate?.aggregate?.avg?.views);
 
-  const avgEr = (!isNaN(usersCount) && !isNaN(views) && views > 0 && usersCount > 0) ? ((views / usersCount) * 100) : 0
+  const avgEr = !isNaN(usersCount) && !isNaN(views) && views > 0 && usersCount > 0 ? (views / usersCount) * 100 : 0;
 
   return {
     avgErQuery,
